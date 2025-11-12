e7bca9b29cb1882bc4bedd9b13c3a1b8fe43da96
LenaAshqar <s202282660@kfupm.edu.sa>
2025-11-12 11:27:24 +0300

    to do 3

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -125,8 +125,12 @@ app.get("/echo", (req,res)=>{
 });
 
 
-
 // Route params: /profile/First/Last
+app.get("/profile/:first/:last", (req,res)=>{
+    const { first, last } = req.params;
+    return { ok:true, fullName: "<first> <last>" }
+});
+
 
 // Route param middleware example: /users/42
 
