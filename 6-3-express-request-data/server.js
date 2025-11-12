c746994c724a4a91bf77aa6080fcaf0a4c74e9ba
LenaAshqar <s202282660@kfupm.edu.sa>
2025-11-12 11:32:51 +0300

    to do 5

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -145,6 +145,8 @@ app.param("userId", (req,res,next,userId)=>{
 });
 
 // Route params: /users/:userId route
-
+app.get("/users/:userId", (req,res)=>{
+        return { ok:true, userId: req.userIdNum }
+    });
 
 
